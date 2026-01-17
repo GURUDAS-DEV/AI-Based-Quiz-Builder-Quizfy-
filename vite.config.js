7dd570f5215d36c2a430b9f1cac4c1600c00599d
Gurudas Bhardwaj <gursad5@gmail.com>
2026-01-17 15:52:45 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',                 // 🔥 THIS FIXES ASSETS
   plugins: [tailwindcss()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true
+    host: '0.0.0.0',     
+    port: 5173,           
+    strictPort: true    
   }
 })
